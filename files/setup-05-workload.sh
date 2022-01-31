3d6ae16c2c08e2c3b835c35a1939bcb8e57ffcd9
Thomas Seufert <tseufert@gmail.com>
2022-01-31 16:06:26 -0500

    init cluster mgmt branch

--- files/setup-05-workload.sh
+++ files/setup-05-workload.sh
@@ -1,3 +1,11 @@
 #!/bin/bash 
 
 kubectl apply -f /root/setup/cluster-management.yaml
+
+if [ -z "${GIT_REPO}" ]
+#if no repo is set then use imgpkg config
+then
+   cat /root/setup/imgpkg-config.yaml | envsubst - |  kubectl apply -f -
+else
+    cat /root/setup/git-config.yaml | envsubst - |  kubectl apply -f -
+fi
