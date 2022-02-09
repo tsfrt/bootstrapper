e87ef8d32083b2f7b001439bc7de69a5a0287409
Thomas Seufert <tseufert@gmail.com>
2022-02-08 23:16:12 -0500

    fix apply

--- files/setup-05-workload.sh
+++ files/setup-05-workload.sh
@@ -5,7 +5,7 @@ kubectl apply -f /root/setup/cluster-management.yaml
 if [ -z "${GIT_REPO}" ]
 #if no repo is set then use imgpkg config
 then
-   cat /root/setup/imgpkg-config.yaml | envsubst - |  kubectl apply -f -
+  envsubst < <(cat /root/setup/imgpkg-config.yaml) | kubectl apply -f - 
 else
-    cat /root/setup/git-config.yaml | envsubst - |  kubectl apply -f -
+  envsubst < <(cat /root/setup/git-config.yaml) | kubectl apply -f - 
 fi
